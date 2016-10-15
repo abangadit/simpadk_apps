f749e052a35ffee1511c92cbe3a0d71c2e6b1694
abangadit <abangadit@abangadit-macbook.local>
2016-10-15 13:45:00 +0700

    cart done, proses insert ke order sudah, tapi ke order_item ada bugs di sp nya

--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -2,7 +2,7 @@
  * Master Controller
  */
 var base_url = "http://localhost:3000/";
-var api_key = "d19ea5b21a429e672e9bffa04114bb2f7616"; //api key sementara (belum ada login)
+var api_key = "d19ea5b21a429e672e9affa04617b92c741c"; //api key sementara (belum ada login)
 
 angular.module('RDash')
     .controller('MasterCtrl', ['$rootScope','$scope', '$cookieStore', '$http', MasterCtrl]);
--- src/js/controllers/transaksi-ctrl.js
+++ src/js/controllers/transaksi-ctrl.js
@@ -2,88 +2,225 @@
  * Alerts Controller
  */
 
-angular
-    .module('RDash')
-    .service('TransaksiService', function () {
+ angular
+     .module('RDash')
+     .service('productService', function () {
 
-    })
-	.controller('TransaksiCtrl', ['$timeout','$rootScope','$scope', '$http','$state','$uibModal','TransaksiService', function ($timeout,$rootScope,$scope,$http,$state,$uibModal,$log,MerkService) {
-		$scope.sample_item = ['1','2','3','4','5','6','7','8','9','10'];
-		var page = 1;
-		$scope.cart = [];
-		$scope.items = ['item1', 'item2', 'item3'];
-		$scope.animationsEnabled = true;
+     })
+     .service('cartService', function () {
+       return {
+               form: [],
+               getForm: function() {
+                   return this.form;
+               },
+               updateForm: function(item) {
+                   this.form.push(item);
+               },
+               clearForm: function() {
+                   this.form = [];
+               }
+           }
+     })
+  	.controller('TransaksiCtrl', ['$timeout','$rootScope','$scope', '$http','$state','productService','cartService','$uibModal', function ($timeout,$rootScope,$scope,$http,$state,productService,cartService,$uibModal) {
+      var page = 1;
+  		$scope.sample_item = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
+  		$scope.items = ['item1', 'item2', 'item3'];
+  		$scope.animationsEnabled = true;
+      $scope.nama_customer = "Pelanggan Biasa";
+      $scope.alamat_customer = " - ";
+      $scope.telp_customer = " - ";
+      $scope.cart = cartService.getForm();
+      $scope.customer_id = 0;
+      $scope.total = 0;
+      //$scope.cart = $scope.sample_item;
 
 
-		$rootScope.isLoading = true;
-		$http.get(base_url+"api/"+api_key+"/products/0/"+page)
-		.then(function(response) {
-			$timeout( function(){
-        		$rootScope.isLoading = false;
-	        }, 1000);
-			
-			console.log(response);
-			$scope.data_product = response.data;
-		});
 
-		$scope.order_popup = function(size,id,name){
-			var modalInstance = $uibModal.open({
-		        templateUrl: 'templates/myModalContent.html',
-		        size: size,
-		        resolve: {
-		            items: function () {
-		                return $scope.items;
-		            }
-		        }
-		    });
 
-		    modalInstance.result.then(function (selectedItem) {
-		        $scope.selected = selectedItem;
-		    }, function () {
-		        $log.info('Modal dismissed at: ' + new Date());
-		    });
-		}
 
-		$scope.order = function(id,name){
-			//alert(id);
-			var lanjut = true;
-			if($scope.cart.length>0){
-				for(var i in $scope.cart){
-					//if(lanjut===true){
-					console.log($scope.cart[i].id +"==="+ id);
-				    if($scope.cart[i].id === id){
-			         	console.log("found "+id);
-			         	$scope.cart[i].qty += 1; 
-			         }else{
-			         	console.log("belum ada bro");
-			         	$scope.cart.push({
-							id  : id,
-							name: name,
-							qty : 1
-						});
-			         }
-					//}
-				}
-				console.log("lanjut "+lanjut);
-			}else{
-				$scope.cart.push({
-					id  : id,
-					name: name,
-					qty : 1
-				});
-			}
-			console.log($scope.cart);
-		};
-	}])
-	.controller('ModalInstanceCtrl', ['$timeout','$rootScope','$scope', '$http','$state','$uibModal','TransaksiService', function ($timeout,$rootScope,$scope,$http,$state,$uibModal,$log,MerkService) {
-		$scope.ok = function () {
-			console.log("ok");
-		    $uibModal.close();
+      //get products katalog
+  		$rootScope.isLoading = true;
+  		$http.get(base_url+"api/"+api_key+"/products/0/"+page)
+  		.then(function(response) {
+  			$timeout( function(){
+          		$rootScope.isLoading = false;
+  	        }, 1000);
+
+  			console.log(response);
+  			$scope.data_product = response.data;
+  		});
+
+      //kosongkan cart
+      $scope.emptyCart = function(){
+        console.log("kosongkan");
+        cartService.clearForm();
+        $scope.cart = cartService.getForm();
+        $scope.total = 0;
+      }
+
+      //tampilkan popup order
+  		$scope.order_popup = function(size,id,name,price){
+  			productService.product_id=id;
+    		productService.product_name=name;
+    		productService.product_price=price;
+
+        //alert(name+" "+productService.product_price);
+        open_popup(size);
+  		}
+
+      //tampilkan popup pilih customer
+      $scope.pilih_customer = function (){
+        $rootScope.modalInstance = $uibModal.open({
+            templateUrl: 'templates/myModalCustomer.html',
+            size: "md",
+        });
+      }
+
+      function open_popup (size){
+        $rootScope.modalInstance = $uibModal.open({
+  	        templateUrl: 'templates/myModalContent.html',
+  	        size: size,
+  	        resolve: {
+  	            items: function () {
+  	                return $scope.items;
+  	            }
+  	        }
+  	    });
+
+  	    $rootScope.modalInstance.result.then(function (selectedItem) {
+  	        $scope.selected = selectedItem;
+
+            $scope.total += selectedItem;
+  	    }, function () {
+  	        //$log.info('Modal dismissed at: ' + new Date());
+  	    });
+      }
+
+      //proses checkout
+  		$scope.checkout = function(){
+        $rootScope.isLoading = true;
+        $scope.order_count = 0;
+  			$http({
+  				method: 'POST',
+  				headers  : { 'Content-Type': 'application/x-www-form-urlencoded' },
+  				url 	 : base_url+"api/"+api_key+"/orders",
+  				data 	 : {
+  					order_id:0,
+  					order_date:new Date(),
+  					user_id:1,
+  					customer_id:$scope.customer_id,
+  					total:$scope.total,
+  					discount:0,
+  					grand_total:$scope.total,
+  					delivery_date:"",
+  					isdelivered:0,
+  					remarks:"",
+  					key:api_key
+  				},
+  				transformRequest: function(obj) {
+  					var str = [];
+  					for(var p in obj){
+  					  str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+  					}
+  					return str.join('&');
+  				}
+  			}).then(function successCallback(response) {
+  			      // $timeout( function(){
+  	        	// 	$rootScope.isLoading = false;
+  		        // }, 1000);
+  		        // alert("Pesanan Berhasil Dibuat");
+  		        console.log(response.data[0].insert_id);
+              insert_order_item(response.data[0].insert_id);
+
+  			  }, function errorCallback(response) {
+  			    $timeout( function(){
+  	        		$rootScope.isLoading = false;
+  		        }, 1000);
+  		        alert("Terjadi Kesalahan, Ulangi ");
+  			  });
+
+
+  		};
+
+      function insert_order_item(order_id){
+        console.log("insert order item after order created , order_id:"+order_id);
+        for (var i = 0; i < $scope.cart.length; i++) {
+    			$http({
+    				method: 'POST',
+    				headers  : { 'Content-Type': 'application/x-www-form-urlencoded' },
+    				url 	 : base_url+"api/"+api_key+"/orderitems",
+    				data 	 : {
+    					orderitem_id:0,
+    					order_id:order_id,
+    					customer_id:0,
+    					customer_price_id:0,
+    					product_id:$scope.cart[i].id,
+    					customer_price:$scope.cart[i].price,
+    					quantity:$scope.cart[i].qty,
+    					subtotal:$scope.cart[i].price*$scope.cart[i].qty,
+    					key:api_key
+    				},
+    				transformRequest: function(obj) {
+    					var str = [];
+    					for(var p in obj){
+    					  str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+    					}
+    					return str.join('&');
+    				}
+    			}).then(function successCallback(response) {
+    			      $scope.order_count += 1;
+                order_done();
+    			  }, function errorCallback(response) {
+    		        alert("Terjadi Kesalahan, Ulangi ");
+    			  });
+        }
+      }
+      function order_done(){
+        if($scope.order_count==$scope.cart.length){
+          $timeout( function(){
+            $rootScope.isLoading = false;
+          }, 1000);
+        }
+
+      }
+
+
+  	}])
+  	.controller('ModalCustomerCtrl', ['$timeout','$rootScope','$scope', '$http','$state','productService','cartService','$uibModal', function ($timeout,$rootScope,$scope,$http,$state,productService,cartService,$uibModal,$log,$uibModalInstance) {
+      $rootScope.isLoading = true;
+  		$http.get(base_url+"api/"+api_key+"/customers/0/")
+  		.then(function(response) {
+  			$timeout( function(){
+          		$rootScope.isLoading = false;
+  	        }, 1000);
+
+  			console.log(response);
+  			$scope.data_customer = response.data;
+  		});
+
+  	}])
+  	.controller('ModalInstanceCtrl', ['$timeout','$rootScope','$scope', '$http','$state','productService','cartService','$uibModal', function ($timeout,$rootScope,$scope,$http,$state,productService,cartService,$uibModal,$log,$uibModalInstance) {
+      $scope.productPrice = productService.product_price;
+      $scope.productName = productService.product_name;
+      $scope.productID= productService.product_id;
+  		$scope.postData = {};
+
+  		$scope.ok = function () {
+        if($scope.postData.qty==0 || typeof $scope.postData.qty === "undefined") {
+          alert("Quantitiy Tidak Boleh Kosong ! ");
+        }else{
+          productService.total = $scope.productPrice;
+          cartService.updateForm(angular.copy({
+            id  : $scope.productID,
+            name: $scope.productName,
+            qty : $scope.postData.qty,
+            price : $scope.productPrice * $scope.postData.qty
+          }));
+          $rootScope.modalInstance.close($scope.productPrice * $scope.postData.qty);
+        }
+
 		  };
 
-		  $scope.cancel = function () {
-			console.log("cancel");
-		    $uibModal.dismiss('cancel');
-		  };
-	}])
+
+  	}])
 ;
