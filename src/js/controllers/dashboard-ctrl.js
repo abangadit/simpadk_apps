16e5eab32c2b0d3f186aa83521c8e69611c90c16
abangadit <abangadit@abangadit-macbook.local>
2017-01-06 13:27:25 +0700

    dashboard + login done

--- src/js/controllers/dashboard-ctrl.js
+++ src/js/controllers/dashboard-ctrl.js
@@ -5,35 +5,62 @@
 angular
     .module('RDash')
     .config(['ChartJsProvider', function (ChartJsProvider) {
-	    // Configure all charts 
+	    // Configure all charts
 	    ChartJsProvider.setOptions({
 	      chartColors: ['#FF5252', '#FF8A80'],
 	      responsive: false
 	    });
-	    // Configure all line charts 
+	    // Configure all line charts
 	    ChartJsProvider.setOptions('line', {
 	      showLines: false
 	    });
 	  }])
-	.controller('DashboardCtrl', ['$scope', '$http','$state','$timeout', function ($scope,$http,$state,$timeout) {
+	.controller('DashboardCtrl', ['$scope', '$http','$state','$timeout','$rootScope', function ($scope,$http,$state,$timeout,$rootScope) {
 		console.log("dashboard page");
-		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		$scope.labels = [];
 		$scope.series = ['Series A', 'Series B'];
-		$scope.data = [
-			[65, 59, 80, 81, 56, 55, 40],
-			[28, 48, 40, 19, 86, 27, 90]
-		];
-		$scope.onClick = function (points, evt) {
+		$scope.data = [];
+
+
+    //get widget sales//
+    $rootScope.isLoading = true;
+    $http.get(base_url+"api/"+api_key+"/dashboards/saleswidget")
+    .then(function(response) {
+      $rootScope.isLoading = false;
+
+      $scope.daily = response.data[0].DailySales;
+      $scope.weekly = response.data[0].WeeklySales;
+      $scope.monthly = response.data[0].MonthlySales;
+      console.log(response.data);
+    });
+    //----//
+
+    //get grafik dashboard//
+    $http.get(base_url+"api/"+api_key+"/dashboards/graphic/month")
+    .then(function(response) {
+      $rootScope.isLoading = false;
+
+      for(var x = 1;x<=response.data.length;x++){
+        $scope.labels.push(x);
+        $scope.data.push(response.data[x]);
+      }
+      console.log(response.data);
+    });
+    //----//
+
+    //get peringatan stok dashboard//
+    $http.get(base_url+"api/"+api_key+"/dashboards/stock")
+    .then(function(response) {
+      $rootScope.isLoading = false;
+      $scope.data_stok = response.data;
+      console.log(response.data);
+    });
+    //----//
+
+    $scope.onClick = function (points, evt) {
 			console.log(points, evt);
 		};
 
-		// Simulate async data update 
-		$timeout(function () {
-		$scope.data = [
-		  [28, 48, 40, 19, 86, 27, 90],
-		  [65, 59, 80, 81, 56, 55, 40]
-		];
-		}, 3000);
 	}])
 
 
--- src/js/controllers/master-ctrl.js
+++ src/js/controllers/master-ctrl.js
@@ -3,15 +3,20 @@
  */
 var base_url = "http://localhost:3000/";
 var base_url_main = "http://localhost:6699/";
-var api_key = "c89ba5b91d1ed6642892ffa04716bc2a76118831ee"; //api key sementara (belum ada login)
+var api_key = "c89ba5b91d1ed6642892fea1431cba28731680"; //api key sementara (belum ada login)
 
 angular.module('RDash')
-    .controller('MasterCtrl', ['$rootScope','$scope', '$cookieStore', '$http', MasterCtrl]);
+    .controller('MasterCtrl', ['$rootScope','$scope', '$cookieStore', '$http','$timeout', MasterCtrl]);
 
-function MasterCtrl($rootScope,$scope, $cookieStore) {
-    /**
-     * Sidebar Toggle & Cookie Control
-     */
+function MasterCtrl($rootScope,$scope,$cookieStore,$http,$timeout) {
+    $scope.postData = {};
+    $scope.isLogin = window.localStorage['isLogin'];
+    $scope.user_nama = window.localStorage['user_nama'];
+    $scope.user_level = window.localStorage['user_level'];
+    $scope.expired_date = window.localStorage['expired_date'];
+    $scope.apikey = window.localStorage['apikey'];
+    $scope.isLogin = window.localStorage['isLogin'];
+    
     $rootScope.isLoading = false;
     var mobileView = 992;
 
@@ -40,4 +45,72 @@ function MasterCtrl($rootScope,$scope, $cookieStore) {
     window.onresize = function() {
         $scope.$apply();
     };
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $scope.submitLogin = function (){
+      $rootScope.isLoading = true;
+      var link = base_url+"login";
+      console.log(link);
+      $http({
+				method: 'POST',
+				headers  : { 'Content-Type': 'application/x-www-form-urlencoded' },
+        url 	 : link,
+				data 	 : {
+					user_nama:$scope.postData.username,
+					password:$scope.postData.password,
+				},
+				transformRequest: function(obj) {
+					var str = [];
+					for(var p in obj){
+					  str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+					}
+					return str.join('&');
+				}
+			}).then(function successCallback(response) {
+          $timeout( function(){
+            $rootScope.isLoading = false;
+          }, 1000);
+          if(response.data=="Invalid user/password"){
+            alert(response.data);
+          }else{
+            $scope.isLogin = true;
+            window.localStorage['isLogin'] = true;
+            window.localStorage['user_nama'] = $scope.postData.username;
+            window.localStorage['apikey'] = response.data.apikey;
+            window.localStorage['user_level'] = response.data.user_level;
+            window.localStorage['expired_date'] = response.data.expired_date;
+
+            $scope.user_nama = window.localStorage['user_nama'];
+            $scope.user_level = window.localStorage['user_level'];
+            $scope.expired_date = window.localStorage['expired_date'];
+            $scope.apikey = window.localStorage['apikey'];
+            $scope.isLogin = window.localStorage['isLogin'];
+          }
+
+
+			  }, function errorCallback(response) {
+			    $timeout( function(){
+	        	$rootScope.isLoading = false;
+		      }, 1000);
+		      alert("Terjadi Kesalahan, Ulangi ");
+			  });
+
+		};
+
+    $scope.logout = function(){
+        $scope.isLogin = false;
+        localStorage.clear();
+    }
+
 }
